8670434123321ff410de5671c32d33c1412f1613
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-26 13:13:50 +0300

    Update card.js

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,11 @@ const Card = (props) => {
   const { name, id, email } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="Robots" />
+      <img
+        src={`https://robohash.org/${id}`}
+        alt="Robots"
+        style={{ width: 200, height: 200 }}
+      />
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
