18740c67ce36dee0bc572b92e63f4273029bbb88
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-27 10:23:42 +0300

    Update cardlist

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((item) => {
+        return (
+          <Card
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            email={item.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
